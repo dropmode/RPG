379846d71770905f5af293b48786eed0a3eaedbd
MItsuki Shishido <169013117+dropmode@users.noreply.github.com>
2024-05-22 13:31:21 +0900

    2024．5.22②

---
+++ src/Humans/Wizard.java
@@ -0,0 +1,25 @@
+package humans;
+
+import bases.Human;
+import utils.Dice;
+
+public class Wizard extends Human {
+	public Wizard(String name, String weapon) {
+		super(name, weapon);
+		setHp(HP);
+		setOffensive(OFFENSIVE);
+	}
+
+	int HP = Dice.get(120, 180);
+
+	public void setHp(int HP) {
+		this.hp = HP;
+	}
+
+	int OFFENSIVE = Dice.get(12, 18);
+
+	public void setOffensive(int OFFENSIVE) {
+		this.offensive = OFFENSIVE;
+	}
+
+}
